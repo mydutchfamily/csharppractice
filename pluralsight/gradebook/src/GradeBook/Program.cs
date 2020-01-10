75f12d8070f968b4aad65bd1c34b370cc295a7cd
mydutchfamily <mydutchfamily1@gmail.com>
2020-01-10 23:08:12 +0200

    Adding numbers

---
+++ pluralsight/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 34.1;
+            double y = 10.3;
+            var result = x + y;
+
+            var numbers = new double[3];
+
+            System.Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello!");
+            }
+        }
+    }
+}
