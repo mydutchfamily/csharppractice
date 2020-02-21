f74f5854f15a52bce4b77ee98954c40d48996785
OLBR <OLBR@simcorp.com>
2020-02-21 13:13:11 +0200

    C# Best Pract: Lazy 'Creating', Elvis null checking, obj init 3, reference module

--- pluralsight/best-practices-improving-basics/2-csharp-best-practices-improving-basics-m2-exercise-files/AcmeApp/Acme.Biz/Product.cs
+++ pluralsight/best-practices-improving-basics/2-csharp-best-practices-improving-basics-m2-exercise-files/AcmeApp/Acme.Biz/Product.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
+using static Acme.Common.LoggingService;
 
 namespace Acme.Biz
 {
@@ -11,9 +13,11 @@ namespace Acme.Biz
     /// </summary>
     public class Product
     {
+        #region Constructors
         public Product()
         {
             Console.WriteLine("Product instance created");
+           //this.ProductVendor = new Vendor();
         }
 
         public Product(string productName, string description, int productId):this()
@@ -24,6 +28,24 @@ namespace Acme.Biz
 
             Console.WriteLine("Product instance has a name: "+ productName);
         }
+        #endregion
+
+        #region Properties
+        private Vendor productVendor;
+
+        public Vendor ProductVendor
+        {
+            get
+            {
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                }
+                return productVendor;
+            }
+            set { productVendor = value; }
+        }
+
 
         private string productName;
 
@@ -48,9 +70,17 @@ namespace Acme.Biz
             get { return productId; }
             set { productId = value; }
         }
-
+#endregion
         public string SayHello()
         {
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product");
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Product", this.productName, "sales@abc.com");
+
+            var result = LogAction("saying hello");
+
             return "Hello " + ProductName + " (" + ProductId + "): " + Description;
         }
 
--- pluralsight/best-practices-improving-basics/2-csharp-best-practices-improving-basics-m2-exercise-files/AcmeApp/Tests/Acme.BizTests/ProductTests.cs
+++ pluralsight/best-practices-improving-basics/2-csharp-best-practices-improving-basics-m2-exercise-files/AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -19,6 +19,7 @@ namespace Acme.BizTests
             currentProduct.ProductName = "Saw";
             currentProduct.ProductId = 1;
             currentProduct.Description = "15-inch steel blade hand saw";
+            currentProduct.ProductVendor.CompanyName = "ABC Corp";
             var expected = "Hello Saw (1): 15-inch steel blade hand saw";
 
             Assert.AreEqual(expected, currentProduct.SayHello());
@@ -33,5 +34,31 @@ namespace Acme.BizTests
 
             Assert.AreEqual(expected, currentProduct.SayHello());
         }
+
+        [TestMethod()]
+        public void SayHello_ObjectInitializer()
+        {
+            var currentProduct = new Product {// in did default constructor will be used and parameters set, no need in custom constructor
+               ProductName = "Saw",
+                Description = "15-inch steel blade hand saw",
+                ProductId = 1
+            };
+
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+
+            Assert.AreEqual(expected, currentProduct.SayHello());
+        }
+
+        [TestMethod()]
+        public void Product_Null()
+        {
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            Assert.AreEqual(expected, companyName);
+        }
+
     }
 }
