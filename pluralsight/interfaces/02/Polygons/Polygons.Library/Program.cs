911da3855178c7c6182f6fc422d168e298385ce1
OLBR <OLBR@simcorp.com>
2020-02-26 17:25:29 +0200

    C# Interfaces: Introduction (Virtual, Abstract, Interface)

---
+++ pluralsight/interfaces/02/Polygons/Polygons.Library/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons.Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ pluralsight/interfaces/02/Polygons/Polygons.Library/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace Polygons.Library
+{
+    public class Square : ConcreteRegularPolygon
+    {
+        public Square(int length) :
+            base(4, length)
+        { }
+
+        public override double GetArea()
+        //public double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
