b0a9e5d169f5cfb1fd6752cc1873a0b7a5a91f1d
mydutchfamily <mydutchfamily1@gmail.com>
2020-03-03 23:01:37 +0200

    Basics Unit Tests: create class for test

---
+++ pluralsight/basics_unit_tests/MyClasses/MyClasses/FileProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName) {
+            if (string.IsNullOrEmpty(fileName)) {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            return File.Exists(fileName);
+        }
+            
+    }
+}
