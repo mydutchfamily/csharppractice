f8d16ecd8d72392be0c6d6bce5c71dd86e15c9ca
mydutchfamily <mydutchfamily1@gmail.com>
2020-03-31 11:49:53 +0300

    C# Events, Delegates, EventArgs, Anonymouse methods, Delegate inference

---
+++ pluralsight/delegates_lambda/EventsDelegatesLambdas/WorkPerformedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    public class WorkPerformedEventArgs: EventArgs
+    {
+        public WorkPerformedEventArgs(int hours, WorkType workType) {
+            Hours = hours;
+            WorkType = workType;
+        }
+        public int Hours { get; set; }
+        public WorkType WorkType{ get; set; }
+    }
+}
---
+++ pluralsight/delegates_lambda/EventsDelegatesLambdas/Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event WorkPerformedHandler2 WorkPerformed2;
+        public event WorkCompletedHandler WorkCompleted;
+
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed3;
+        public event EventHandler WorkCompleted2;
+        
+        public void DoWork(int hours, WorkType workType) {
+            for (int i = 0; i<hours; i++) {
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i+1,workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType) {
+            if (WorkPerformed != null)
+            {
+                WorkPerformed(hours, workType);
+            }
+
+            var del2 = WorkPerformed2 as WorkPerformedHandler2;// can cast to delegate
+            if (del2 != null)
+            {// listeners are attached
+                del2(this, new WorkPerformedEventArgs(hours, workType));
+            }
+
+            var del3 = WorkPerformed3 as EventHandler<WorkPerformedEventArgs>;
+            if (del3 != null) {// listeners are attached
+                del3(this, new WorkPerformedEventArgs(hours, workType));
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            if (WorkCompleted != null)
+            {
+                WorkCompleted(this, EventArgs.Empty);
+            }
+
+            EventHandler del = WorkCompleted2 as EventHandler;
+            if (del != null)
+            {// listeners are attached
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
