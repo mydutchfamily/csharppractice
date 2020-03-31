98e0916cc06f50b8dad345271debab5a8d6d0024
mydutchfamily <mydutchfamily1@gmail.com>
2020-03-31 12:36:48 +0300

    C# Events, Delegates: Lambdas

---
+++ pluralsight/delegates_lambda/EventsDelegatesLambdas/ProcessData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    class ProcessData
+    {
+        public void Process(int x, int y, BizRuleDelegate del) {
+            var result = del(x, y);
+            Console.WriteLine(nameof(Process) + ": " + result);
+        }
+    }
+}
