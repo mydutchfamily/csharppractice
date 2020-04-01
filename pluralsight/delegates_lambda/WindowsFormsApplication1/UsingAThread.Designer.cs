dd52c548a27f5165f7726600fb62a7a34d7cc790
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-01 11:29:30 +0300

    C# Events, Delegates: Thread

---
+++ pluralsight/delegates_lambda/WindowsFormsApplication1/UsingAThread.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class UsingAThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(11, 16);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(109, 34);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 76);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 36);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(144, 23);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(35, 13);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.Text = "label1";
+            // 
+            // UsingAThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "UsingAThread";
+            this.Text = "UsingAThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
---
+++ pluralsight/delegates_lambda/WindowsFormsApplication1/UsingAThread.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class UsingAThread : Form
+    {
+        private int _Max;
+        private delegate void StartProcessDelegate();
+        public UsingAThread()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new UsingAThread());
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            _Max = 100;
+            var t = new Thread(new ThreadStart(StartProcess));
+            t.Start();
+        }
+
+        private void StartProcess() {
+            if (progressBar1.InvokeRequired)// switch from one thread to another
+            {
+                var del = new StartProcessDelegate(StartProcess); // do call from gui thread to non gui
+                this.Invoke(del);
+            } else {
+                // will fail if execute the code from GUI thread
+                this.Refresh();
+                this.progressBar1.Maximum = _Max;
+                for (int i = 0; i <= _Max; i++)
+                {
+                    Thread.Sleep(10);
+                    this.labelOutput.Text = i.ToString();
+                    this.progressBar1.Value = i;
+                }
+            }
+
+        }
+    }
+}
