c3895bb627f10bd19a985e2475cb178fc2918ec2
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-12 13:13:40 +0300

    Advanced Collections: SortedDictionary - sort by key

--- pluralsight/advanced_collections/TourBooker/AppData.cs
+++ pluralsight/advanced_collections/TourBooker/AppData.cs
@@ -10,11 +10,15 @@ namespace TourBooker.Logic
     {
         public List<Country> AllCountries { get; private set; }
         public Dictionary<string, Country> AllCountriesByKey { get; private set;}
+        public SortedDictionary<string, Country> AllCountriesByKeySorted { get; private set; }
         public void Initialize(string csvFilePath) {
             CsvReader reader = new CsvReader(csvFilePath);
             this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
 
             this.AllCountriesByKey = AllCountries.ToDictionary(x => x.Code, StringComparer.OrdinalIgnoreCase); // add statndart comparator to ignore case 
+
+            this.AllCountriesByKeySorted = new SortedDictionary<string, Country>(AllCountriesByKey);
+
         }
     }
 }
