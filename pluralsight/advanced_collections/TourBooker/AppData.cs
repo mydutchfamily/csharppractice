08f762b5b7fd351f768270b484eb50ab66dc5024
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-12 12:43:34 +0300

    Advanced Collections: all countries with List<Country>

---
+++ pluralsight/advanced_collections/TourBooker/AppData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourBooker.Logic
+{
+    public class AppData
+    {
+        public List<Country> AllCountries { get; private set; }
+        public Dictionary<string, Country> AllCountriesByKey { get; private set;}
+        public void Initialize(string csvFilePath) {
+            CsvReader reader = new CsvReader(csvFilePath);
+            this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
+
+            this.AllCountriesByKey = AllCountries.ToDictionary(x => x.Code);
+        }
+    }
+}
---
+++ pluralsight/advanced_collections/TourBooker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourBooker.Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
