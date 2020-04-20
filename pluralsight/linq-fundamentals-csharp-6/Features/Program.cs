051794eb829a332488712298837d4e099feb56a4
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-20 15:20:28 +0300

    LINQ Fundamentals: IEnumerable, IEnumerator

---
+++ pluralsight/linq-fundamentals-csharp-6/Features/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+                {
+                    new Employee { Id = 1, Name = "Scott"},
+                    new Employee { Id = 2, Name = "Chris"}
+                };
+
+            IEnumerable<Employee> sales = new List<Employee>() {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+
+            while (enumerator.MoveNext()) {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+        }
+    }
+}
