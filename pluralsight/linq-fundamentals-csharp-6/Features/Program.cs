7a478a5d769ed89d58b67ca11a6e51b68dbc4b41
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-20 16:21:12 +0300

    LINQ Fundamentals: Func/Action delegates, Query vs Method syntax

--- pluralsight/linq-fundamentals-csharp-6/Features/Program.cs
+++ pluralsight/linq-fundamentals-csharp-6/Features/Program.cs
@@ -10,6 +10,23 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Func<int, int> square = x => x*x; // last int is type for return
+            Console.WriteLine(square(3));
+
+            Func<int, int, int> add =  (x,y) => x + y;
+            Func<int, int, int> add2 = (x, y) =>
+            {
+                int z = x + y;
+                return z;
+            };
+
+            Console.WriteLine(square(add(3, add2(2,2))));
+
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(3, add2(2, 2))));
+
+            Console.WriteLine("*****************");
+
             IEnumerable<Employee> developers = new Employee[]
                 {
                     new Employee { Id = 1, Name = "Scott"},
@@ -48,6 +65,31 @@ namespace Features
                 Console.WriteLine(item.Name);
             }
 
+            Console.WriteLine("*****************");
+            foreach (var item in developers.Where(arg => arg.Name.Length == 5)
+                                            .OrderBy(e => e.Name))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("*****************");
+            var query = developers.Where(arg => arg.Name.Length == 5)
+                                            .OrderBy(e => e.Name);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("*****************");
+            var query2 = from dev in developers
+                        where dev.Name.Length == 5
+                        orderby dev.Name
+                        select dev;
+
+            foreach (var item in query2)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
 
         private static bool NameStartsWithS(Employee arg)
