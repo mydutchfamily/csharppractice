019052fac1748a5bc8fde583e3dbfa0f24571beb
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-20 12:03:29 +0300

    LINQ Fundamentals: list largest files without LINQ

--- pluralsight/advanced_collections/TourBooker/AppData.cs
+++ pluralsight/advanced_collections/TourBooker/AppData.cs
@@ -12,8 +12,10 @@ namespace TourBooker.Logic
         public Dictionary<string, Country> AllCountriesByKey { get; private set;}
         public Dictionary<CountryCode, Country> AllCountriesCCByKey { get; private set; }
         public SortedDictionary<string, Country> AllCountriesByKeySorted { get; private set; }
-
         public SortedList<string, Country> AllCountriesByKeySortedList { get; private set; }
+
+        public LinkedList<Country> ItineraryBuilder { get; } = new LinkedList<Country>();
+        public SortedDictionary<string, Tour> AllTurs { get; private set; } = new SortedDictionary<string, Tour>();
         public void Initialize(string csvFilePath) {
             CsvReader reader = new CsvReader(csvFilePath);
             this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
@@ -25,6 +27,8 @@ namespace TourBooker.Logic
             this.AllCountriesByKeySorted = new SortedDictionary<string, Country>(AllCountriesByKey);
 
             this.AllCountriesByKeySortedList = new SortedList<string, Country>(AllCountriesByKey);
+
+
 
         }
     }
