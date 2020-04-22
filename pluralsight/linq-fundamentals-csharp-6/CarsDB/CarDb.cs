91c8d2fc3e2e396d8c2c2c4bc9da683c00736709
mydutchfamily <mydutchfamily1@gmail.com>
2020-04-22 21:27:19 +0300

    LINQ Fundamentals: install entity framework, create local database, parse file and insert records/class into table

---
+++ pluralsight/linq-fundamentals-csharp-6/CarsDB/CarDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsDB
+{
+    public class CarDb: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ pluralsight/linq-fundamentals-csharp-6/CarsDB/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDb>());// force framework recreate db if something has changed
+            InsertData();
+            QueryData();
+        }
+
+        private static void QueryData()
+        {
+            
+        }
+
+        private static void InsertData()
+        {
+            var cars = ProcessCars("fuel.csv");
+            var db = new CarDb();// by default will try to connect to local db
+
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();               
+            }
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+            var query =
+
+                File.ReadAllLines(path)
+                    .Skip(1)// first line is header
+                    .Where(l => l.Length > 1)// last line is empty
+                    .ToCar();
+
+            return query.ToList();
+        }
+
+
+    }
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car// return new Car each time in loop iteration
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3], System.Globalization.CultureInfo.InvariantCulture),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+    }
+}
