e96ff1cca6e3aae7e526a8e000c5e94096d02dda
mydutchfamily <mydutchfamily1@gmail.com>
2020-05-03 20:26:37 +0300

    Equality and Comparisons: virtual Equals(), static Equals(), static ReferenceEquals(), class vs struct

---
+++ pluralsight/equality_and_comparisons/FoodEquals/Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEquals
+{
+    public class Food
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public Food(string name)
+        {
+            this._name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+
+    public struct FoodStruct
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public FoodStruct(string name)
+        {
+            this._name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
---
+++ pluralsight/equality_and_comparisons/FoodEquals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));// virtual object.Equals()
+            Console.WriteLine(object.Equals(banana, banana2));// static object.Equals() - if one of the objects is null
+            Console.WriteLine(object.ReferenceEquals(banana, banana2));// ALWAYS check reference to same instance
+
+            Console.WriteLine("********************************************");
+
+            FoodStruct bananas = new FoodStruct("banana");
+            FoodStruct bananas2 = new FoodStruct("banana");
+
+            Console.WriteLine(bananas.Equals(bananas2));
+
+            Console.ReadLine();
+        }
+    }
+}
