fe0930ca4d21ae27a108a763ead604032ab0416e
mydutchfamily <mydutchfamily1@gmail.com>
2020-05-05 08:46:39 +0300

    Equality and Comparisons: IComparer<T>.Compare(x,y), Comparer<T>.Comparer(x,y) as singletone - to sort array

---
+++ pluralsight/equality_and_comparisons/Comparers/FoodNameComparer.cs
@@ -0,0 +1,42 @@
+﻿using FoodEquals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparers
+{
+    class FoodNameIComparer : IComparer<Food>
+    {
+        public int Compare(Food x, Food y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+            return string.Compare(x.Name, y.Name, StringComparison.CurrentCulture);
+        }
+    }
+
+    class FoodNameComparer : Comparer<Food>
+    {
+        private static FoodNameComparer _instance = new FoodNameComparer();
+
+        public static FoodNameComparer Instance { get { return _instance; } }// one instance is enough
+
+        private FoodNameComparer() { }// hide constructor;
+        public override int Compare(Food x, Food y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+            return string.Compare(x.Name, y.Name, StringComparison.CurrentCulture);
+        }
+    }
+}
---
+++ pluralsight/equality_and_comparisons/Comparers/Program.cs
@@ -0,0 +1,44 @@
+﻿using FoodEquals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food[] list = {
+                new Food("orange", FoodGroup.Fruit),
+                new Food("banana", FoodGroup.Fruit),
+                new Food("pear", FoodGroup.Fruit),
+                new Food("apple", FoodGroup.Fruit)
+            };
+
+
+            Array.Sort(list, FoodNameComparer.Instance);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("*********************************************");
+
+
+
+            Array.Sort(list, new FoodNameIComparer());
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
