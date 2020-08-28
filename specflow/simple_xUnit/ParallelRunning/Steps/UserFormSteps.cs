b0980fa93c9954162a81c6b9cd6cb0bfe6d56b18
OLBR <OLBR@simcorp.com>
2020-08-28 17:18:19 +0300

    Specflow: part 25 - Table1.FindInSet|FindAllInSet(ListData) - return matched record(s)

--- specflow/simple_xUnit/ParallelRunning/Steps/UserFormSteps.cs
+++ specflow/simple_xUnit/ParallelRunning/Steps/UserFormSteps.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 using Xunit.Abstractions;
 
 namespace ParallelRunning.Steps
@@ -19,23 +21,68 @@ namespace ParallelRunning.Steps
         {
             output.WriteLine("GivenINavigateToApplication ");
         }
-        
+
         [Given(@"I click login")]
         public void GivenIClickLogin()
         {
             output.WriteLine("GivenIClickLogin ");
         }
-        
+
         [Given(@"I start entering user form details like")]
         public void GivenIStartEnteringUserFormDetailsLike(Table table)
         {
             output.WriteLine("GivenIStartEnteringUserFormDetailsLike ");
         }
-        
+
         [Given(@"I click submit button")]
         public void GivenIClickSubmitButton()
         {
             output.WriteLine("GivenIClickSubmitButton ");
         }
+
+        [Given(@"I verify the entered user form details in the application database")]
+        public void GivenIVerifyTheEnteredUserFormDetailsInTheApplicationDatabase(Table table)
+        {
+            //Mock data
+            List<AUTDatabase> mockAUTData = new List<AUTDatabase>()
+            {
+                new AUTDatabase() {
+                    FirstName = "Karthik",
+                    Initial = "k",
+                    MiddleName = "k"
+                }
+            };
+
+            var result = table.FindInSet(mockAUTData);
+
+            //Mock data
+            mockAUTData = new List<AUTDatabase>()
+            {
+                new AUTDatabase() {
+                    FirstName = "Karthik",
+                    Initial = "k",
+                    MiddleName = "k"
+                },
+                new AUTDatabase() {
+                    FirstName = "Prashanth",
+                    Initial = "k",
+                    MiddleName = "k"
+                }
+            };
+
+            var resultAll = table.FindAllInSet(mockAUTData);// will return matching rows
+        }
+
+    }
+
+    public class AUTDatabase
+    {
+        public string Initial { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Gender { get; set; }
     }
 }
